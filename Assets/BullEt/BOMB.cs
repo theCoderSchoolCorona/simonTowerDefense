4ac187fb26d9d93ab6a38a67e82ebe9cd1f05f01
theCoderSchool at Corona <155676852+theCoderSchoolCorona@users.noreply.github.com>
2026-02-11 16:21:52 -0800

    made retry button

--- Assets/BullEt/BOMB.cs
+++ Assets/BullEt/BOMB.cs
@@ -6,7 +6,6 @@ public class BOMB : projectile
     public GameObject BOX;
     public override void explode()
     {
-        Debug.Log("HEHEHEHEHE");
         var evil = Instantiate(BOX, transform.position, transform.rotation);
         evil.GetComponent<projectile>().Pain = Pain;
         base.explode();
